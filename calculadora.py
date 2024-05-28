671f6b1be728ab47b8595f37c8100e369cee1ad9
Melisolares2005 <msolares@scl.edu.gt>
2024-05-28 13:37:56 -0600

    Prueba Corta 1
    Se completa la opción del portafolio de Python.

--- calculadora.py
+++ calculadora.py
@@ -4,7 +4,7 @@
 
 
 def sumar(num1, num2):
-    return num1 + num2;
+    return num1 + num2
 
 # @resultado = 15
 resultado = sumar(10,5) #return 10+5=15
@@ -13,19 +13,19 @@ print(resultado)
 
 
 def restar(a, b):
-    return a - b;
+    return a - b
 resultado = restar(15,5) 
 print(resultado)
 
 
 def multiplicar(a, b):
-    return a * b;
+    return a * b
 resultado = multiplicar(5,5) 
 print(resultado)
 
 
 def dividir(a, b):
-    return a / b;
+    return a / b
 resultado = dividir(25,5) 
 print(resultado)
 
@@ -34,10 +34,26 @@ print(resultado)
 # --------------------
 
 def programa_calculadora():
-    print("[1] -----------------------------------------\n")
+    print("[1] ------------------------------")
     print(f"[1] Sumar           [2] Restar")
-    print(f"[3] Multiplicar     [4] Dividir")
+    print(f"[3] Multiplicar     [4] Dividir\n")
 
+    opcion = input("[?]: ")
     
+    num1 = int(input("[Num 1]: "))
+    num2 = int(input("[Num 2]: "))
+
+    if opcion == '1':
+        print("[Res +]: ", sumar(num1,num2))
+    elif opcion == '2':
+        print("[Res -]: ", restar(num1,num2))
+    elif opcion == '3':
+        print("[Res *]: ", multiplicar(num1,num2))
+    elif opcion == '4':
+        print("[Res /]: ", dividir(num1,num2))
+    
+    print("----------------------------------")
+
+
 
 
--- main.py
+++ main.py
@@ -6,14 +6,17 @@ from calculadora import programa_calculadora
 # INICIO DEL PROGRAMA
 os.system("cls")
 
-cargar_opciones()
 
-# EJECUTO MI PTOGRAMA ESPERANDO UN POSIBLE ERROR
-try:  # SI NO HAY ERRORES
-    respuesta = input("[?] ")
+while True:
+    cargar_opciones()
+    # EJECUTO MI PTOGRAMA ESPERANDO UN POSIBLE ERROR
+    try:  # SI NO HAY ERRORES
+        respuesta = input("[?] ")
 
-    if respuesta == "1":
-        programa_calculadora()
+        if respuesta == "1":
+            programa_calculadora()
+        elif respuesta == "10":
+            break
     
-except:  # SI HAY ERROR
-    print("valor nulo")
+    except:  # SI HAY ERROR
+        print("valor nulo")
