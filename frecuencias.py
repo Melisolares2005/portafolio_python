a35fd9165837bd350181c09d26d4f2f002ab9272
Melisolares2005 <msolares@scl.edu.gt>
2024-06-18 13:43:09 -0600

    Prueba Corta #3
    18 de junio de 2024

---
+++ frecuencias.py
@@ -0,0 +1,21 @@
+# COMENTARIOS POR CADA LINEA DE CÓDIGO
+# Función
+def grafica_frecuencias(lista):
+    # Iterar cada elemento de la lista
+    for p in lista:
+        # Imprimir el nombre del producto agregando un espacio
+        print(p["producto"]," ",end="")
+        # Iterar desde 0 hasta la frecuencua en el diccionario p
+        for f in range(p["f"]):
+            # Imprimir 0 en cada iteración agregando un espacio
+            print("0",end="")
+            # Imprimir un nuevo espacio
+        print("")
+
+
+
+   
+
+
+        
+    
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from lista_opciones import *
 from calculadora import programa_calculadora
 from conversor import *
+from frecuencias import *
 
 # INICIO DEL PROGRAMA
 os.system("cls")
@@ -10,7 +11,7 @@ os.system("cls")
 # lista de opciones
 lista_de_opciones = [
     "Calculadora",
-    "Conversor Monedas",
+    "Graficar Frecuencias",
     "Opción 3",
     "Opción 4",
     "Opción 5",
@@ -31,6 +32,14 @@ while True:
             programa_calculadora()
         elif respuesta == "2":
             conversor_de_monedas()
+        elif respuesta == "3":
+            lista = [
+                {"producto":"Tomate   ","f":20},
+                {"producto":"Coliflor ","f":30},
+                {"producto":"Espinaca ","f":25},
+                {"producto":"Apio     ","f":35}
+            ]
+            grafica_frecuencias(lista)
         elif respuesta == "10":
             print("[ FIN DEL PROGRAMA]")
             break
